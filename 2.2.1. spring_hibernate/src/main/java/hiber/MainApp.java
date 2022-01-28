4c22db1b4618346daa13257cf29e92c78a0f3481
JZawR <94790420+JZawR@users.noreply.github.com>
2022-01-28 13:46:28 +0300

    commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,11 +1,13 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApp {
@@ -15,18 +17,22 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("lada", 1)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("BMW", 2)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("nissan", 3)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("audi", 4)));
+
+      // Достаем Пользователя по машине
+      try {
+         System.out.println(userService.getUserByCar(new Car("nissan", 3)));
+      } catch (IndexOutOfBoundsException e) {
+         System.out.println("\n Не нашлось :( \n");
+      }
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println(user);
       }
 
       context.close();
